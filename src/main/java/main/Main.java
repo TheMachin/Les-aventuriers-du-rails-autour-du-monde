460211027811f07f8a7efa76fa855aa3be09a44b
TheMachin <thibaultmartinec@gmail.com>
2017-04-04 09:36:18 +0200

    View - Problem intregation chemin

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import vue.DeckController;
+
+public class Main extends Application{
+
+	
+	private Stage primaryStage;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage=primaryStage;
+		this.primaryStage.setTitle("Les aventuriers du rails autour du monde");
+		
+		
+        Parent root = FXMLLoader.load(getClass().getResource("../vue/plateau.fxml"));
+		Scene scene = new Scene(root);
+		this.primaryStage.setScene(scene);
+		this.primaryStage.show();
+		
+		DeckController deckControlle = new DeckController();
+		deckControlle.setMain(this);
+		
+	}
+
+}
